2ea91ee08a31716e5a633ad16da693f18078afc4
anshu kumari <197anshukumari@gmail.in>
2023-01-13 21:57:40 +0530

    Quick Changer

---
+++ app/src/main/java/com/coding/a12quickchanger/MainActivity.java
@@ -0,0 +1,39 @@
+package com.coding.a12quickchanger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+    RelativeLayout layout;
+    RadioGroup radioGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        layout = findViewById(R.id.layout);
+        radioGroup= findViewById(R.id.rgroup);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedID) {
+                switch (checkedID) {
+                    case R.id.rbtnblue:
+                        layout.setBackgroundColor(Color.parseColor("#3944F7"));
+                        break;
+                    case R.id.rbtngreen:
+                        layout.setBackgroundColor(Color.parseColor("#45CE30"));
+                        break;
+                    case R.id.rbtnred:
+                        layout.setBackgroundColor(Color.parseColor("#FF3E40"));
+                        break;
+                }
+            }
+        });
+    }
+}
